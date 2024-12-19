c395b708baee014321e37df1cf4cd674247357d7
konraddrozdyk <konrad.drozdyk@gmail.com>
2024-12-19 11:15:11 +0100

    configured tailwindcss

--- game-of-minds-fe/src/App.tsx
+++ game-of-minds-fe/src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="text-9xl bg-slate-300">Hej</div>
+    </>
+  );
 }
 
 export default App;
